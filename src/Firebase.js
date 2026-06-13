52b6bd5ff4414694882b365df70eeea7c1e22ed3
jsmccann618 <jsmccann618@gmail.com>
2026-06-13 15:36:08 -0400

    Add Firebase Storage initialization

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDJo9nnOVt0hSR6YdngyuZhiimdzvg6n7A",
@@ -12,3 +13,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+export const storage = getStorage(app);
