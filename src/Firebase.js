204d54244b647556b9c557a4430b53534a817ced
jsmccann618 <jsmccann618@gmail.com>
2026-06-13 15:19:20 -0400

    Add Firebase initialization and Firestore setup

---
+++ src/Firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDJo9nnOVt0hSR6YdngyuZhiimdzvg6n7A",
+  authDomain: "wildlife-journal-978fa.firebaseapp.com",
+  projectId: "wildlife-journal-978fa",
+  storageBucket: "wildlife-journal-978fa.firebasestorage.app",
+  messagingSenderId: "456351309551",
+  appId: "1:456351309551:web:6f560942ea3e4fd9b7451a"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
